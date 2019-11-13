f79bde759ea3096a20f8327dff2d666ff55ddc0f
oliver <usernamewlawlawla@gmail.com>
2019-11-13 11:55:44 +0630

    awww

---
+++ app/src/main/java/wla/googoodolls/specialcalculator/ui/HtwetList.kt
@@ -0,0 +1,13 @@
+package wla.googoodolls.specialcalculator.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wla.googoodolls.specialcalculator.R
+
+class HtwetList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_htwet_list)
+    }
+}
---
+++ app/src/main/java/wla/googoodolls/specialcalculator/ui/PoutList.kt
@@ -0,0 +1,13 @@
+package wla.googoodolls.specialcalculator.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wla.googoodolls.specialcalculator.R
+
+class PoutList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pout_list)
+    }
+}
